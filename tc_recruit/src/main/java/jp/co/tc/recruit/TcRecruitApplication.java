5cc87bbd04bcb2da2470520687189d18b848f543
YumeaMatsubara <y.matsubara@time-creators.com>
2020-04-23 18:18:17 +0900

    0423

--- tc_recruit/src/main/java/jp/co/tc/recruit/TcRecruitApplication.java
+++ tc_recruit/src/main/java/jp/co/tc/recruit/TcRecruitApplication.java
@@ -2,10 +2,11 @@ package jp.co.tc.recruit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-/*@EntityScan("jp.co.tc.recruit.entity")//←今回追加したアノテーション
-@EnableJpaRepositories("jp.co.tc.recruit.repository")*/
+@EntityScan("jp.co.tc.recruit.entity")//←今回追加したアノテーション
+/*@EnableJpaRepositories("jp.co.tc.recruit.repository")*/
 public class TcRecruitApplication {
 
 	public static void main(String[] args) {
